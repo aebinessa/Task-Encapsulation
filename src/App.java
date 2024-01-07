ff5f01aca3975be44aa92238b4eea435ce04f187
aebinessa <abinessa0004@sdsu.edu>
2024-01-07 11:59:13 +0300

    fixes logic in getgpastatus, add setters and gette rs for other variables

--- src/App.java
+++ src/App.java
@@ -1,10 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Student student = new Student();
-        student.setGpa(3);
+        student.setGpa(4.0);
         String gpaStatus = student.getGpaStatus();
+        student.setName("yousef");
+        String studentName = student.getStudentName();
+        student.setAge(21);
 
-        System.out.println("the student's gpa status is " + gpaStatus);
+        System.out.println("the student's gpa " + gpaStatus);
+        System.out.println("the student's name is" + studentName); // one way of doing it, calling a string
+        System.out.println("the student's gpa is " + student.getStudentAge()); // the other way of doing it,
+                                                                               // calling the getter from the student
+                                                                               // class
 
     }
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,25 +1,44 @@
 public class Student {
     private String name;
     private int age;
-    private double gpa;
+    private double gpa = 0.0;
 
     public void setGpa(double gpa) {
 
         if (gpa < 0.0 || gpa > 4.0) {
             gpa = 0.0;
+        } else {
+            this.gpa = gpa;
         }
     }
 
     public String getGpaStatus() {
-        gpa = 0;
+        // gpa = 0;, was resetting the code therefore conditions were not working
         String gpaStatus = null;
         if (gpa > 3) {
             gpaStatus = "Excellent";
-        } else if (gpa > 2 && gpa < 3) {
+        } else if (gpa >= 2.0) {
             gpaStatus = "Good";
-        } else if (gpa < 2) {
+        } else {
             gpaStatus = "Needs Imporovment";
         }
         return gpaStatus;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStudentAge() {
+        return age;
+    }
+
 }
